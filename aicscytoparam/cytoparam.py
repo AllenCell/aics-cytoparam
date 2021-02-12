700fd3c91e9bf22a657a8fd27049bc7bf3829b91
Matheus Viana <vianamp@gmail.com>
2021-02-12 14:06:28 -0800

    flake8 fix

--- aicscytoparam/cytoparam.py
+++ aicscytoparam/cytoparam.py
@@ -7,6 +7,7 @@ from scipy import interpolate as spinterp
 from typing import Optional, List, Dict, Tuple
 from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 
+
 def parameterize_image_coordinates(
     seg_mem: np.array,
     seg_nuc: np.array,
@@ -29,7 +30,7 @@ def parameterize_image_coordinates(
     nisos : list
         [a,b] representing the number of layers that will be used to
         parameterize the nucleoplasm and cytoplasm.
-        
+
     Returns
     -------
     coords: np.array
@@ -45,42 +46,43 @@ def parameterize_image_coordinates(
     centroid_nuc: tuple
         (x,y,z) representing nuclear centroid.
     """
-    
+
     if (seg_mem.dtype != np.uint8) or (seg_nuc.dtype != np.uint8):
         warnings.warn("One or more input images is not an 8-bit image\
         and will be cast to 8-bit.")
-    
+
     # Cell SHE coefficients
-    (coeffs_mem, _), (_, _, _, centroid_mem)= shparam.get_shcoeffs(
-        image = seg_mem,
-        lmax = lmax,
-        sigma = 0,
-        compute_lcc = True,
-        alignment_2d = False)
+    (coeffs_mem, _), (_, _, _, centroid_mem) = shparam.get_shcoeffs(
+        image=seg_mem,
+        lmax=lmax,
+        sigma=0,
+        compute_lcc=True,
+        alignment_2d=False)
 
     # Nuclear SHE coefficients
-    (coeffs_nuc, _), (_, _, _, centroid_nuc)= shparam.get_shcoeffs(
-        image = seg_nuc,
-        lmax = lmax,
-        sigma = 0,
-        compute_lcc = True,
-        alignment_2d = False)
+    (coeffs_nuc, _), (_, _, _, centroid_nuc) = shparam.get_shcoeffs(
+        image=seg_nuc,
+        lmax=lmax,
+        sigma=0,
+        compute_lcc=True,
+        alignment_2d=False)
 
     # Get Coordinates
     coords = get_mapping_coordinates(
-        coeffs_mem = coeffs_mem,
-        centroid_mem = centroid_mem,
-        coeffs_nuc = coeffs_nuc,
-        centroid_nuc = centroid_nuc,
-        nisos = nisos
+        coeffs_mem=coeffs_mem,
+        centroid_mem=centroid_mem,
+        coeffs_nuc=coeffs_nuc,
+        centroid_nuc=centroid_nuc,
+        nisos=nisos
     )
 
     # Shift coordinates to the center of the input
     # segmentations
-    coords += np.array(centroid_mem).reshape(3,1,1)
-    
+    coords += np.array(centroid_mem).reshape(3, 1, 1)
+
     return coords, (coeffs_mem, centroid_mem, coeffs_nuc, centroid_nuc)
-    
+
+
 def parameterization_from_shcoeffs(
     coeffs_mem: Dict,
     centroid_mem: List,
@@ -89,7 +91,7 @@ def parameterization_from_shcoeffs(
     nisos: List,
     images_to_probe: Optional[List] = None,
 ):
-    
+
     """
     Runs the parameterization for a cell represented by its spherical
     harmonics coefficients calculated by using ther package aics-shparam.
@@ -123,19 +125,20 @@ def parameterization_from_shcoeffs(
     """
 
     if len(coeffs_mem) != len(coeffs_nuc):
-        raise ErrorValue(f"Number of cell and nuclear coefficients do not\
-        match: {len(coeffs_mem)} and {len(coeffs_nuc)}.")
-    
+        raise ValueError(f"Number of cell and nuclear coefficients\
+        do not match: {len(coeffs_mem)} and {len(coeffs_nuc)}.")
+
     representations = cellular_mapping(
-        coeffs_mem = coeffs_mem,
-        centroid_mem = centroid_mem,
-        coeffs_nuc = coeffs_nuc,
-        centroid_nuc = centroid_nuc,
-        nisos = nisos,
-        images_to_probe = images_to_probe,
+        coeffs_mem=coeffs_mem,
+        centroid_mem=centroid_mem,
+        coeffs_nuc=coeffs_nuc,
+        centroid_nuc=centroid_nuc,
+        nisos=nisos,
+        images_to_probe=images_to_probe,
     )
 
     return representations
+
 
 def get_interpolators(
     coeffs_mem: Dict,
@@ -144,7 +147,7 @@ def get_interpolators(
     centroid_nuc: Dict,
     nisos: List
 ):
-    
+
     """
     Creates 1D interpolators for SHE coefficients with fixed points
     at: 1) nuclear centroid, 2) nuclear shell and 3) cell membrane.
@@ -163,7 +166,7 @@ def get_interpolators(
     nisos : list
         [a,b] representing the number of layers that will be used to
         parameterize the nucleoplasm and cytoplasm.
-        
+
     Returns
     -------
         coeffs_interpolator: spinterp.interp1d
@@ -172,30 +175,31 @@ def get_interpolators(
     """
 
     if len(coeffs_mem) != len(coeffs_nuc):
-        raise ErrorValue(f"Number of cell and nuclear coefficients do not\
-        match: {len(coeffs_mem)} and {len(coeffs_nuc)}.")
-    
+        raise ValueError(f"Number of cell and nuclear coefficients\
+        do not match: {len(coeffs_mem)} and {len(coeffs_nuc)}.")
+
     # Total number of coefficients
     nc = len(coeffs_mem)
     # Degree of the expansion (lmax)
-    lmax = int(np.sqrt(nc/2.)-1)
-    
+    lmax = int(np.sqrt(nc / 2.) - 1)
+
     if nc != len(coeffs_nuc):
-        raise ValueError(f"Number of coefficients in mem_coeffs and nuc_coeffs are different:{nc,len(coeffs_nuc)}")
+        raise ValueError(f"Number of coefficients in mem_coeffs and\
+        nuc_coeffs are different:{nc,len(coeffs_nuc)}")
 
     # Concatenate centroid into same array for interpolation
-    centroids = np.c_[centroid_nuc,centroid_nuc,centroid_mem]
-        
+    centroids = np.c_[centroid_nuc, centroid_nuc, centroid_mem]
+
     # Array to be interpolated
     coeffs_ctr_arr = np.array([0 if i else 1 for i in range(nc)])
-    coeffs_mem_arr = np.zeros((2,lmax+1,lmax+1))
-    coeffs_nuc_arr = np.zeros((2,lmax+1,lmax+1))
+    coeffs_mem_arr = np.zeros((2, lmax + 1, lmax + 1))
+    coeffs_nuc_arr = np.zeros((2, lmax + 1, lmax + 1))
     # Populate cell and nuclear arrays and concatenate into a single arr
-    for k, kname in enumerate(['C','S']):
-        for l in range(lmax+1):
-            for m in range(lmax+1):
-                coeffs_mem_arr[k,l,m] = coeffs_mem[f'shcoeffs_L{l}M{m}{kname}']
-                coeffs_nuc_arr[k,l,m] = coeffs_nuc[f'shcoeffs_L{l}M{m}{kname}']
+    for k, kname in enumerate(['C', 'S']):
+        for L in range(lmax + 1):
+            for m in range(lmax + 1):
+                coeffs_mem_arr[k, L, m] = coeffs_mem[f'shcoeffs_L{L}M{m}{kname}']
+                coeffs_nuc_arr[k, L, m] = coeffs_nuc[f'shcoeffs_L{L}M{m}{kname}']
     coeffs_mem_arr = coeffs_mem_arr.flatten()
     coeffs_nuc_arr = coeffs_nuc_arr.flatten()
     coeffs = np.c_[coeffs_ctr_arr, coeffs_nuc_arr, coeffs_mem_arr]
@@ -212,7 +216,8 @@ def get_interpolators(
     centroids_interpolator = spinterp.interp1d(iso_values, centroids)
 
     return coeffs_interpolator, centroids_interpolator, lmax
-    
+
+
 def get_mapping_coordinates(
     coeffs_mem: Dict,
     centroid_mem: List,
@@ -220,7 +225,7 @@ def get_mapping_coordinates(
     centroid_nuc: List,
     nisos: List
 ):
-    
+
     """
     Interpolate spherical harmonics coefficients representing the nuclear centroid,
     the nuclear shell and the cell membrane. As the coefficients are interpolated,
@@ -240,7 +245,7 @@ def get_mapping_coordinates(
     nisos : list
         [a,b] representing the number of layers that will be used to
         parameterize the nucleoplasm and cytoplasm.
-        
+
     Returns
     -------
     coords: np.array
@@ -250,34 +255,35 @@ def get_mapping_coordinates(
     """
 
     if len(coeffs_mem) != len(coeffs_nuc):
-        raise ErrorValue(f"Number of cell and nuclear coefficients do not\
+        raise ValueError(f"Number of cell and nuclear coefficients do not\
         match: {len(coeffs_mem)} and {len(coeffs_nuc)}.")
-    
+
     # Get interpolators
     coeffs_interpolator, centroids_interpolator, lmax = get_interpolators(
-        coeffs_mem = coeffs_mem,
-        centroid_mem = centroid_mem,
-        coeffs_nuc = coeffs_nuc,
-        centroid_nuc = centroid_nuc,
-        nisos = nisos
+        coeffs_mem=coeffs_mem,
+        centroid_mem=centroid_mem,
+        coeffs_nuc=coeffs_nuc,
+        centroid_nuc=centroid_nuc,
+        nisos=nisos
     )
-    
+
     x_coords, y_coords, z_coords = [], [], []
-    for i, iso_value in enumerate(np.linspace(0.0,1.0,1+np.sum(nisos))):
-        
+    for i, iso_value in enumerate(np.linspace(0.0, 1.0, 1 + np.sum(nisos))):
+
         # Get coeffs at given fixed point
-        coeffs = coeffs_interpolator(iso_value).reshape(2,lmax+1,lmax+1)
-        mesh, _ = shtools.get_reconstruction_from_coeffs(coeffs, lrec=2*lmax)
-        
+        coeffs = coeffs_interpolator(iso_value).reshape(2, lmax + 1, lmax + 1)
+        mesh, _ = shtools.get_reconstruction_from_coeffs(coeffs, lrec=2 * lmax)
+
         # Store coordinates
         coords = vtk_to_numpy(mesh.GetPoints().GetData())
-        x_coords.append(coords[:,0])
-        y_coords.append(coords[:,1])
-        z_coords.append(coords[:,2])
-        
-    coords = np.array((x_coords,y_coords,z_coords))
-        
+        x_coords.append(coords[:, 0])
+        y_coords.append(coords[:, 1])
+        z_coords.append(coords[:, 2])
+
+    coords = np.array((x_coords, y_coords, z_coords))
+
     return coords
+
 
 def cellular_mapping(
     coeffs_mem: Dict,
@@ -323,18 +329,18 @@ def cellular_mapping(
     """
 
     if len(coeffs_mem) != len(coeffs_nuc):
-        raise ErrorValue(f"Number of cell and nuclear coefficients do not\
+        raise ValueError(f"Number of cell and nuclear coefficients do not\
         match: {len(coeffs_mem)} and {len(coeffs_nuc)}.")
 
     # Get interpolators
     coeffs_interpolator, centroids_interpolator, lmax = get_interpolators(
-        coeffs_mem = coeffs_mem,
-        centroid_mem = centroid_mem,
-        coeffs_nuc = coeffs_nuc,
-        centroid_nuc = centroid_nuc,
-        nisos = nisos
+        coeffs_mem=coeffs_mem,
+        centroid_mem=centroid_mem,
+        coeffs_nuc=coeffs_nuc,
+        centroid_nuc=centroid_nuc,
+        nisos=nisos
     )
-    
+
     representations = []
     for i, iso_value in enumerate(np.linspace(0.0, 1.0, 1 + np.sum(nisos))):
 
@@ -347,10 +353,11 @@ def cellular_mapping(
         coords = vtk_to_numpy(mesh.GetPoints().GetData())
         coords += centroid
         mesh.GetPoints().SetData(numpy_to_vtk(coords))
-        
+
         # Probe images of interest to create representation
         rep = get_intensity_representation(
-            polydata=mesh, images_to_probe=images_to_probe
+            polydata=mesh,
+            images_to_probe=images_to_probe
         )
 
         representations.append(rep)
@@ -377,6 +384,7 @@ def cellular_mapping(
     code.channel_names = ch_names
 
     return code
+
 
 def get_intensity_representation(
     polydata: vtk.vtkPolyData,
@@ -418,6 +426,7 @@ def get_intensity_representation(
 
     return representation
 
+
 def voxelize_mesh(
     imagedata: vtk.vtkImageData,
     shape: Tuple,
@@ -427,7 +436,7 @@ def voxelize_mesh(
 
     """
     Voxelize a triangle mesh into an image.
-    
+
     Parameters
     --------------------
     imagedata: vtkImageData
@@ -439,13 +448,13 @@ def voxelize_mesh(
         Mesh to be voxelized
     origin: List
         xyz specifying the lower left corner of the mesh.
-        
+
     Returns
     -------
     img: np.array
         Binary array.
     """
-    
+
     pol2stenc = vtk.vtkPolyDataToImageStencil()
     pol2stenc.SetInputData(mesh)
     pol2stenc.SetOutputOrigin(origin)
@@ -465,6 +474,7 @@ def voxelize_mesh(
 
     return img
 
+
 def voxelize_meshes(
     meshes: List
 ):
@@ -473,7 +483,7 @@ def voxelize_meshes(
     List of meshes to be voxelized into an image. Usually
     the input corresponds to the cell membrane and nuclear
     shell meshes.
-    
+
     Parameters
     --------------------
     meshes: List
@@ -490,7 +500,7 @@ def voxelize_meshes(
     origin:
         Origin of the meshes in the voxelized image.
     """
-    
+
     # 1st mesh is used as reference (cell) and it should be
     # the larger than the 2nd one (nucleus).
     mesh = meshes[0]
@@ -499,13 +509,13 @@ def voxelize_meshes(
     coords = vtk_to_numpy(mesh.GetPoints().GetData())
 
     # Find bounds of the mesh
-    rmin = (coords.min(axis=0)-0.5).astype(np.int)
-    rmax = (coords.max(axis=0)+0.5).astype(np.int)
+    rmin = (coords.min(axis=0) - 0.5).astype(np.int)
+    rmax = (coords.max(axis=0) + 0.5).astype(np.int)
 
     # Width, height and depth
-    w = int(2 + (rmax[0]-rmin[0]))
-    h = int(2 + (rmax[1]-rmin[1]))
-    d = int(2 + (rmax[2]-rmin[2]))
+    w = int(2 + (rmax[0] - rmin[0]))
+    h = int(2 + (rmax[1] - rmin[1]))
+    d = int(2 + (rmax[2] - rmin[2]))
 
     # Create image data
     imagedata = vtk.vtkImageData()
@@ -515,8 +525,8 @@ def voxelize_meshes(
     imagedata.AllocateScalars(vtk.VTK_UNSIGNED_CHAR, 1)
 
     # Set all values to 1
-    imagedata.GetPointData().GetScalars().FillComponent(0,1)
-    
+    imagedata.GetPointData().GetScalars().FillComponent(0, 1)
+
     # Create an empty 3D numpy array to sum up
     # voxelization of all meshes
     img = np.zeros((d, h, w), dtype=np.uint8)
@@ -524,29 +534,30 @@ def voxelize_meshes(
     # Voxelize one mesh at the time
     for mid, mesh in enumerate(meshes):
         seg = voxelize_mesh(
-            imagedata = imagedata,
-            shape = (d, h, w),
-            mesh = mesh,
-            origin = rmin
+            imagedata=imagedata,
+            shape=(d, h, w),
+            mesh=mesh,
+            origin=rmin
         )
-        img[seg>0] = mid+1
+        img[seg > 0] = mid + 1
 
     # Origin of the reference system in the image
-    origin = rmin.reshape(1,3)
-    
+    origin = rmin.reshape(1, 3)
+
     return img, origin
+
 
 def morph_representation_on_shape(
     img: np.array,
     param_img_coords: np.array,
     representation: np.array
 ):
-    
+
     """
     Decodes the parameterized intensity representation
     into an image. To do so, an input image with the final
     shape is required.
-    
+
     Parameters
     --------------------
     img: np.array
@@ -569,7 +580,7 @@ def morph_representation_on_shape(
     """
 
     if param_img_coords.shape[1:] != representation.shape:
-        raise ErrorValue(f"Parameterized image coordinates of\
+        raise ValueError(f"Parameterized image coordinates of\
         shape {param_img_coords.shape} and representation of\
         shape {representation.shape} are expected to have the\
         same shape.")
@@ -577,16 +588,16 @@ def morph_representation_on_shape(
     # Reshape coords from 3NM to 3P, where N is the number of
     # isovalues in the representation and M is the number of
     # points in each mesh used to create the representation.
-    param_img_coords = param_img_coords.reshape(3,-1).T
+    param_img_coords = param_img_coords.reshape(3, -1).T
     # Convert XYZ to ZYX
-    param_img_coords = param_img_coords[:,::-1]
+    param_img_coords = param_img_coords[:, ::-1]
     # Reshape representation from NM to P.
     representation = representation.flatten()
-    
+
     # Create a nearest neighbor interpolator
     nninterpolator = spinterp.NearestNDInterpolator(param_img_coords, representation)
     # Interpolate on all foreground voxels
-    cell = np.where(img>0)
+    cell = np.where(img > 0)
     img = img.astype(np.float32)
     img[cell] = nninterpolator(cell)
 

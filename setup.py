5a0b243e0a15fc06bcb5971175203fef2159bcfb
Matheus Viana <vianamp@gmail.com>
2023-04-18 10:36:11 -0700

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -89,6 +89,6 @@ setup(
     url="https://github.com/AllenCell/aics-cytoparam",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.8",
+    version="0.1.9",
     zip_safe=False,
 )

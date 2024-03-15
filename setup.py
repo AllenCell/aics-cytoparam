7c810b81d20eab318156e90142b03c2881003fdf
Matheus Viana <vianamp@gmail.com>
2024-03-15 12:08:41 -0700

    Upgrade to python 3.9+

--- setup.py
+++ setup.py
@@ -9,42 +9,36 @@ with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup_requirements = [
-    "pytest-runner>=5.2",
+    "pytest-runner",
 ]
 
 test_requirements = [
-    "black>=19.10b0",
-    # "codecov>=2.1.4",
-    "flake8>=3.8.3",
-    "flake8-debugger>=3.2.1",
-    "pytest>=5.4.3",
-    "pytest-cov>=2.9.0",
-    "pytest-raises>=0.11",
+    "black",
+    "flake8",
+    "flake8-debugger",
+    "pytest",
+    "pytest-cov",
+    "pytest-raises",
 ]
 
 dev_requirements = [
     *setup_requirements,
     *test_requirements,
-    "bump2version>=1.0.1",
-    "coverage>=5.1",
-    "ipython>=7.15.0",
-    "m2r2>=0.2.7",
-    "pytest-runner>=5.2",
-    "Sphinx>=3.4.3",
-    "sphinx_rtd_theme>=0.5.1",
-    "tox>=3.15.2",
-    "twine>=3.1.1",
-    "wheel>=0.34.2",
+    "bump2version",
+    "coverage",
+    "ipython",
+    "m2r2",
+    "pytest-runner",
+    "Sphinx",
+    "sphinx_rtd_theme",
+    "tox",
+    "twine",
+    "wheel",
 ]
 
 requirements = [
-    "aicsshparam>=0.0.14",
-    "tqdm",
-    "numpy",
-    "vtk>=9.0.0",
-    "aicsimageio",
-    "scipy",
-    "scikit-image>=0.15.0"
+    "aicsshparam",
+    "aicsimageio"
 ]
 
 extra_requirements = {
@@ -61,11 +55,11 @@ setup(
     author="Matheus Viana",
     author_email="matheus.viana@alleninstitute.org",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
         "License :: Free for non-commercial use",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only"
     ],
     description="Cytoplasm parameterization using spherical harmonics",
     entry_points={

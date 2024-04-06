58f02bcb649b1ed42e26b28a9fcd59af434fd77d
Matheus Viana <vianamp@gmail.com>
2024-04-06 08:55:47 -0700

    Added alignment functionalities

---
+++ aicscytoparam/alignment/generic_2d_shape.py
@@ -0,0 +1,145 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import spatial as spspatial
+from skimage import transform as sktrans
+from skimage import measure as skmeasure
+
+class Generic2DShape():
+    """
+    Generic class for 2D shapes
+    """
+    def __init__():
+        pass
+
+    def _compute_contour(self):
+        """
+        Compute the contour of the shape
+        """
+        cont = skmeasure.find_contours(self._polygon)[0]
+        cx, cy = cont[:, 1], cont[:, 0]
+        self.cx = cx - cx.mean()
+        self.cy = cy - cy.mean()
+        return
+    
+    def show(self, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots()
+        ax.plot(self.cx, self.cy)
+        if ax is None:
+            plt.show()
+        
+    def find_angle_that_minimizes_countour_distance(self, cx, cy):
+        """
+        Find the angle that minimizes the distance between the shape and the contour (cx, cy)
+        Parameters
+        ----------
+        cx: np.ndarray
+            x coordinates of the contour
+        cy: np.ndarray
+            y coordinates of the contour
+        Returns
+        -------
+        angle: int
+            angle that minimizes the distance
+        dist: float
+            minimum distance
+        """
+        # Assumes cx and cy are centered at origin
+        dists = []
+        for theta in range(180):
+            cx2rot, cy2rot = Generic2DShape.rotate_contour(cx, cy, theta)
+            X = np.c_[self.cx, self.cy]
+            Y = np.c_[cx2rot, cy2rot]
+            D = spspatial.distance.cdist(X, Y)
+            dist_min = D.min(axis=0).mean() + D.min(axis=1).mean()
+            dists.append(dist_min)
+        return np.argmin(dists), np.min(dists)
+        
+    @staticmethod
+    def rotate_contour(cx, cy, theta):
+        """
+        Rotate a contour around the origin
+        Parameters
+        ----------
+        cx: np.ndarray
+            x coordinates of the contour
+        cy: np.ndarray
+            y coordinates of the contour
+        theta: float
+            angle of rotation
+        Returns
+        -------
+        cxrot: np.ndarray
+            x coordinates of the rotated contour
+        cyrot: np.ndarray
+            y coordinates of the rotated contour
+        """
+        cxrot = cx*np.cos(np.deg2rad(theta)) - cy*np.sin(np.deg2rad(theta))
+        cyrot = cx*np.sin(np.deg2rad(theta)) + cy*np.cos(np.deg2rad(theta))
+        return cxrot, cyrot
+
+    @staticmethod
+    def get_contour_from_3d_image(image, pad=5, center=True):
+        """
+        Get the contour of a 3D image
+        Parameters
+        ----------
+        image: np.ndarray
+            3D image
+        pad: int
+            padding
+        center: bool
+            center the contour
+        Returns
+        -------
+        cx: np.ndarray
+            x coordinates of the contour
+        cy: np.ndarray
+            y coordinates of the contour
+        """
+        mip = image.max(axis=0)
+        y, x = np.where(mip>0)
+        mip = np.pad(mip, ((pad,pad), (pad,pad)))
+        cont = skmeasure.find_contours(mip>0)[0]
+        cx, cy = cont[:, 1], cont[:, 0]
+        if center:
+            cx = cx - cx.mean()
+            cy = cy - cy.mean()
+        return (cx, cy)
+
+class ElongatedHexagonalShape(Generic2DShape):
+    """
+    Elongated hexagonal shape    
+    """
+    def __init__(self, base, elongation, pad=5):
+        self._pad = pad
+        self._base = base
+        self._height = int(self._base/np.sqrt(2))
+        self._elongation_factor = elongation
+        self._create()
+        self._compute_contour()
+        
+    def _create(self):
+        """
+        Create the elongated hexagonal shape
+        """
+        pad = self._pad
+        triangle = np.tril(np.ones((self._height, self._base)))
+        triangle = sktrans.rotate(triangle, angle=-15, center=(0,0), order=0)
+        rectangle = np.ones((self._height, self._base))
+        for _ in range(self._elongation_factor):
+            rectangle = np.concatenate([rectangle, rectangle[:,:1]], axis=1)
+        upper_half = np.concatenate([triangle[:,::-1],rectangle,triangle], axis=1)
+        hexagon = np.concatenate([upper_half, upper_half[::-1]], axis=0)
+        hexagon = np.pad(hexagon, ((pad,pad), (pad,pad)))
+        self._polygon = hexagon
+        return
+    
+    @staticmethod
+    def get_default_parameters_as_dict(elongation=8, base_ini=24, base_end=64):
+        params = []
+        for wid, w in enumerate(np.linspace(base_ini, base_end, elongation)):
+            for fid, f in enumerate(np.linspace(0, w, elongation)):
+                params.append({"base": int(w), "elongation": int(f)})
+        return params
---
+++ aicscytoparam/utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+from typing import List
+from skimage import io as skio
+from aicsshparam import shtools
+from vtk.util import numpy_support as vtknp
+from aicscytoparam.alignment.generic_2d_shape import Generic2DShape
+
+def load_image_and_align_based_on_shape_library(path, library, nuc_channel=1):
+    """
+    Load an image and align it based on a shape library
+    Parameters
+    ----------
+    path: str
+        path to the image
+    library: ShapeLibrary2D
+        shape library
+    nuc_channel: int
+        channel of the nucleus
+    Returns
+    -------
+    image_rot: np.ndarray
+        aligned image    
+    """
+    image = skio.imread(path)
+    (cx, cy) = Generic2DShape.get_contour_from_3d_image(image[0])
+    _,  _, angle = library.find_best_match(cx, cy)
+    image_rot = shtools.rotate_image_2d(image, -angle)
+    # Apply extra 180 degrees rotation if nucleus is at the right
+    # side of the aligned cell
+    x = np.arange(image_rot.shape[-1])
+    px = image_rot[nuc_channel].sum(axis=(0,1))
+    xpx = (x-x.mean())*px/px.sum()
+    if xpx.sum() > 0:
+        image_rot = shtools.rotate_image_2d(image_rot, 180)
+    return image_rot  
+
+
+def get_voxelized_image_of_mean_shape(vec: List, coeffs_mem: List, coeffs_nuc: List, return_meshes=False):
+    vec = np.array(vec).mean(axis=0)
+    df_coeffs_mem = pd.DataFrame(coeffs_mem)
+    df_coeffs_nuc = pd.DataFrame(coeffs_nuc)
+    df_coeffs_mem_avg = df_coeffs_mem.select_dtypes(include='number').mean(axis=0)
+    df_coeffs_nuc_avg = df_coeffs_nuc.select_dtypes(include='number').mean(axis=0)
+    avg_coeffs_mem = shtools.convert_coeffs_dict_to_matrix(df_coeffs_mem_avg, lmax=32)
+    avg_coeffs_nuc = shtools.convert_coeffs_dict_to_matrix(df_coeffs_nuc_avg, lmax=32)
+    avg_mesh_mem, _ = shtools.get_reconstruction_from_coeffs(avg_coeffs_mem)
+    avg_mesh_nuc, _ = shtools.get_reconstruction_from_coeffs(avg_coeffs_nuc)
+    # Translate nucleus to correct location based on average displacement of nuclear
+    # centroid relative to cell centroid
+    pts = vtknp.vtk_to_numpy(avg_mesh_nuc.GetPoints().GetData())
+    shtools.update_mesh_points(avg_mesh_nuc, pts[:,0]-vec[0], pts[:,1]-vec[1], pts[:,2]-vec[2])
+    # Convert average cell shape into an image
+    avg_image, _ = shtools.voxelize_meshes([avg_mesh_mem, avg_mesh_nuc])
+    if return_meshes:
+        return avg_image, avg_mesh_mem, avg_mesh_nuc
+    return avg_image
